a42cb7549d0983ce7651ba9608a75d3150b52749
Ron Pedde <ron@pedde.com>
2005-05-01 05:09:02 +0000

    missing some files in extra_dist

--- mkdist.sh
+++ mkdist.sh
@@ -3,7 +3,7 @@
 mv configure.in configure.in.mkdist
 cat configure.in.mkdist | sed -e s/AM_INIT_AUTOMAKE.*$/AM_INIT_AUTOMAKE\(mt-daapd,cvs-`date +%Y%m%d`\)/ > configure.in
 ./reconf
-./configure --with-id3tag=/sw
+./configure --with-id3tag=/opt/local
 make dist
 mv configure.in.mkdist configure.in
 
