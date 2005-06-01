35af7991d9f6cfb30fe767ce1cb8606aa335a1d6
Ron Pedde <ron@pedde.com>
2005-06-01 14:55:36 +0000

    Move genre list to scan-mp3.c

--- src/scan-mp3.c
+++ src/scan-mp3.c
@@ -84,6 +84,158 @@ int scan_sample_table[3][4] = {
     { 11025, 12000, 8000, 0 }    /* MPEG 2.5 */
 };
 
+char *scan_winamp_genre[] = {
+    "Blues",              // 0
+    "Classic Rock",
+    "Country",
+    "Dance",
+    "Disco",
+    "Funk",               // 5
+    "Grunge",
+    "Hip-Hop",
+    "Jazz",
+    "Metal",
+    "New Age",            // 10
+    "Oldies",
+    "Other",
+    "Pop",
+    "R&B",
+    "Rap",                // 15
+    "Reggae",
+    "Rock",
+    "Techno",
+    "Industrial",
+    "Alternative",        // 20
+    "Ska",
+    "Death Metal",
+    "Pranks",
+    "Soundtrack",
+    "Euro-Techno",        // 25
+    "Ambient",
+    "Trip-Hop",
+    "Vocal",
+    "Jazz+Funk",
+    "Fusion",             // 30
+    "Trance",
+    "Classical",
+    "Instrumental",
+    "Acid",
+    "House",              // 35
+    "Game",
+    "Sound Clip",
+    "Gospel",
+    "Noise",
+    "AlternRock",         // 40
+    "Bass",
+    "Soul",
+    "Punk",
+    "Space",
+    "Meditative",         // 45
+    "Instrumental Pop",
+    "Instrumental Rock",
+    "Ethnic",
+    "Gothic",
+    "Darkwave",           // 50
+    "Techno-Industrial",
+    "Electronic",
+    "Pop-Folk",
+    "Eurodance",
+    "Dream",              // 55
+    "Southern Rock",
+    "Comedy",
+    "Cult",
+    "Gangsta",
+    "Top 40",             // 60
+    "Christian Rap",
+    "Pop/Funk",
+    "Jungle",
+    "Native American",
+    "Cabaret",            // 65
+    "New Wave",
+    "Psychadelic",
+    "Rave",
+    "Showtunes",
+    "Trailer",            // 70
+    "Lo-Fi",
+    "Tribal",
+    "Acid Punk",
+    "Acid Jazz",
+    "Polka",              // 75
+    "Retro",
+    "Musical",
+    "Rock & Roll",
+    "Hard Rock",
+    "Folk",               // 80
+    "Folk/Rock",
+    "National folk",
+    "Swing",
+    "Fast-fusion",
+    "Bebob",              // 85
+    "Latin",
+    "Revival",
+    "Celtic",
+    "Bluegrass",
+    "Avantgarde",         // 90
+    "Gothic Rock",
+    "Progressive Rock",
+    "Psychedelic Rock",
+    "Symphonic Rock",
+    "Slow Rock",          // 95
+    "Big Band",
+    "Chorus",
+    "Easy Listening",
+    "Acoustic",
+    "Humour",             // 100
+    "Speech",
+    "Chanson",
+    "Opera",
+    "Chamber Music",
+    "Sonata",             // 105 
+    "Symphony",
+    "Booty Bass",
+    "Primus",
+    "Porn Groove",
+    "Satire",             // 110
+    "Slow Jam",
+    "Club",
+    "Tango",
+    "Samba",
+    "Folklore",           // 115
+    "Ballad",
+    "Powder Ballad",
+    "Rhythmic Soul",
+    "Freestyle",
+    "Duet",               // 120
+    "Punk Rock",
+    "Drum Solo",
+    "A Capella",
+    "Euro-House",
+    "Dance Hall",         // 125
+    "Goa",
+    "Drum & Bass",
+    "Club House",
+    "Hardcore",
+    "Terror",             // 130
+    "Indie",
+    "BritPop",
+    "NegerPunk",
+    "Polsk Punk",
+    "Beat",               // 135
+    "Christian Gangsta",
+    "Heavy Metal",
+    "Black Metal",
+    "Crossover",
+    "Contemporary C",     // 140
+    "Christian Rock",
+    "Merengue",
+    "Salsa",
+    "Thrash Metal",
+    "Anime",              // 145
+    "JPop",
+    "SynthPop",
+    "Unknown"
+};
+
 /* Forwards */
 static int scan_mp3_get_mp3tags(char *file, MP3FILE *pmp3);
 static int scan_mp3_get_mp3fileinfo(char *file, MP3FILE *pmp3);
