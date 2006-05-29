69bb8dba8278b3c523e0fd4dc06c594d653536bf
Ron Pedde <ron@pedde.com>
2006-05-29 11:07:19 +0000

    Add resources for all plugins, make Mike's configurator the default configurator for the package build.

---
+++ win32/mt-daapd-res.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by mt-daapd.rc
+//
+#define IDI_APPICON                     5
+#define IDI_ICON1                       201
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        202
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
--- win32/versionize.bat
+++ win32/versionize.bat
@@ -1,20 +1,14 @@
 @echo off
 
 set SUBWC="c:\program files\tortoisesvn\bin\subwcrev.exe"
-if not exist %SUBWC% goto NOSUBWC
+if not exist %SUBWC% set SUBWC=copy
 
 echo Fixing version info...
 %SUBWC% %0\..\.. %0\..\config.h.templ %0\..\config.h 
 %SUBWC% %0\..\.. %0\..\FireflyConfig\AssemblyInfo.cs.templ %0\..\FireflyConfig\AssemblyInfo.cs
 %SUBWC% %0\..\.. %0\..\nsi\mt-daapd.nsi.templ %0\..\nsi\mt-daapd.nsi
 %SUBWC% %0\..\.. %0\..\ssc-ffmpeg.rc.templ %0\..\ssc-ffmpeg.rc
+%SUBWC% %0\..\.. %0\..\mt-daapd.rc.templ %0\..\mt-daapd.rc
+%SUBWC% %0\..\.. %0\..\rsp.rc.templ %0\..\rsp.rc
+%SUBWC% %0\..\.. %0\..\w32-event.rc.templ %0\..\w32-event.rc
 %SUBWC% %0\..\.. %0\..\FireflyShell\version.h.templ %0\..\FireflyShell\version.h
-goto END
-
-:NOSUBWC
-copy %0\..\config.h.templ %0\..\config.h
-copy %0\..\FireflyConfig\AssemblyInfo.cs.templ %0\..\FireflyConfig\AssemblyInfo.cs 
-copy %0\..\nsi\mt-daapd.nsi.templ %0\..\nsi\mt-daapd.nsi
-copy %0\..\.. %0\..\ssc-ffmpeg.rc.templ %0\..\ssc-ffmpeg.rc
-copy %0\..\FireflyShell\version.h.templ %0\..\version.h
-:END
