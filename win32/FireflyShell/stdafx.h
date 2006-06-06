e1d07af8827a59b1bf9ead0d3afb8ff20cd2c2dc
Ron Pedde <ron@pedde.com>
2006-06-06 04:03:50 +0000

    Add manifest for winxp style controls, remove link options from stdafx for old broken version of vs.

--- win32/FireflyShell/stdafx.h
+++ win32/FireflyShell/stdafx.h
@@ -57,6 +57,7 @@ extern CAppModule _Module;
 #define NOTIFY_FOR_THIS_SESSION 0
 #endif
 
+#if _MSC_VER >= 1400
 #if defined _M_IX86
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_IA64
@@ -65,6 +66,7 @@ extern CAppModule _Module;
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #else
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
 #endif
 
 inline void SafeStringCopy(TCHAR *dest, const TCHAR *source, size_t len)
