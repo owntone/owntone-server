4004a0f7df2266d8c144e24aa8130b9e69835f54
Anders Betnér <andbe611 at users.sourceforge.net>
2006-06-07 06:54:24 +0000

    Make prototype.js first test for XMLHttprequest object and then the IE active-x thingies. This forces IE 7 to use the native XMLHttprequest object. (and debugging on gecko browsers easier)

--- admin-root/lib-js/prototype.js
+++ admin-root/lib-js/prototype.js
@@ -549,9 +549,9 @@ var $R = function(start, end, exclusive) {
 var Ajax = {
   getTransport: function() {
     return Try.these(
+      function() {return new XMLHttpRequest()},
       function() {return new ActiveXObject('Msxml2.XMLHTTP')},
-      function() {return new ActiveXObject('Microsoft.XMLHTTP')},
-      function() {return new XMLHttpRequest()}
+      function() {return new ActiveXObject('Microsoft.XMLHTTP')}
     ) || false;
   },
 
