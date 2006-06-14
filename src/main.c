97328767d8aa79fe8ea1ffc15f157b3c6568ee08
Ron Pedde <ron@pedde.com>
2006-06-14 04:33:27 +0000

    Don't do startup scan (unless requested) when scan_interval is set to 0

--- src/main.c
+++ src/main.c
@@ -490,7 +490,8 @@ int main(int argc, char *argv[]) {
     DPRINTF(E_LOG,L_MAIN,"Scanned %d songs in  %d seconds\n",song_count,
             end_time-start_time);
 
-    config.reload = 1; /* force a reload on start */
+    if(conf_get_int("general","rescan_interval",0))
+        config.reload = 1; /* force a reload on start */
 
     while(!config.stop) {
         if((conf_get_int("general","rescan_interval",0) &&
