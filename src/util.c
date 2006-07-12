26e4259332fe90636736b97c0769de9c9cc7149a
Ron Pedde <ron@pedde.com>
2006-07-12 04:39:11 +0000

    split well-used utility functions into util.c

---
+++ src/util.c
@@ -0,0 +1,35 @@
+/*
+ * simple utility functions
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+uint32_t util_djb_hash_block(unsigned char *data, uint32_t len) {
+    uint32_t hash = 5381;
+    unsigned char *pstr = data;
+
+    while(len--) {
+	hash = ((hash << 5) + hash) + *pstr;
+	pstr++;
+    }
+    return hash;
+}
+
+
+uint32_t util_djb_hash_str(char *str) {
+    uint32_t len;
+
+    len = strlen(str);
+    return util_djb_hash_block((unsigned char *)str,len);
+}
+
---
+++ src/util.h
@@ -0,0 +1,24 @@
+/*
+ * simple utility functions
+ */
+
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+
+#include <sys/types.h>
+
+/* simple hashing functions */
+extern uint32_t util_djb_hash_block(unsigned char *data, uint32_t len);
+extern uint32_t util_djb_hash_str(char *str);
+
+
+#endif /* _UTIL_H_ */
+
