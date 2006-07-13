2ea83c6f2e03b6eafcc25c32626384947d7d9b25
Ron Pedde <ron@pedde.com>
2006-07-13 03:13:24 +0000

    Fix problem where version wasn't being created properly on initial db

--- src/db-sql-sqlite2.c
+++ src/db-sql-sqlite2.c
@@ -465,7 +465,7 @@ char *db_sqlite2_initial1 =
 "   subterm         VARCHAR(255)    DEFAULT NULL,\n"
 "   value           VARCHAR(1024)   NOT NULL\n"
 ");\n"
-"insert into config values ('version','10');\n";
+"insert into config values ('version','','10');\n";
 
 char *db_sqlite2_initial2 =
 "create table playlists (\n"
