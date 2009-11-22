3ff067da4ecdc0684bb9bfb75058ef8d3d5fe8d5
Julien BLACHE <jb@jblache.org>
2009-11-22 16:45:13 +0100

    Add itunes_overrides parameter

--- src/conffile.c
+++ src/conffile.c
@@ -55,6 +55,7 @@ static cfg_opt_t sec_library[] =
     CFG_STR("password", NULL, CFGF_NONE),
     CFG_STR_LIST("directories", NULL, CFGF_NONE),
     CFG_STR_LIST("compilations", NULL, CFGF_NONE),
+    CFG_BOOL("itunes_overrides", cfg_false, CFGF_NONE),
     CFG_STR_LIST("no_transcode", NULL, CFGF_NONE),
     CFG_STR_LIST("force_transcode", NULL, CFGF_NONE),
     CFG_END()
