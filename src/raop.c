652fa9371f5902ab3e11e6b259d3c5743ecbf60e
Julien BLACHE <jb@jblache.org>
2011-03-04 21:20:07 +0100

    Add terminal \r\n sequence for RAOP volume control on 802.11g ApEx
    The 802.11g ApEx seems to require a terminal \r\n sequence, otherwise it
    just ignores the setting.

--- src/raop.c
+++ src/raop.c
@@ -1711,7 +1711,7 @@ raop_set_volume_internal(struct raop_session *rs, int volume, evrtsp_req_cb cb)
   raop_volume = raop_volume_convert(volume);
 
   /* Don't let locales get in the way here */
-  ret = evbuffer_add_printf(evbuf, "volume: %d.%06d", (int)raop_volume, -(int)(1000000.0 * (raop_volume - (int)raop_volume)));
+  ret = evbuffer_add_printf(evbuf, "volume: %d.%06d\r\n", (int)raop_volume, -(int)(1000000.0 * (raop_volume - (int)raop_volume)));
   if (ret < 0)
     {
       DPRINTF(E_LOG, L_RAOP, "Out of memory for SET_PARAMETER payload (volume)\n");
