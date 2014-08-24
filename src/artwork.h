d168f25502f4c1e47faab8d94acf1aaf3662c121
chme <christian.meffert@googlemail.com>
2014-08-24 16:43:00 +0200

    get artwork by dbmfi

--- src/artwork.h
+++ src/artwork.h
@@ -14,6 +14,9 @@ int
 artwork_get_item_id(int id, int max_w, int max_h, int format, struct evbuffer *evbuf);
 
 int
+artwork_get_item_dbmfi(struct db_media_file_info *dbmfi, int max_w, int max_h, int format, struct evbuffer *evbuf);
+
+int
 artwork_get_group(int id, int max_w, int max_h, int format, struct evbuffer *evbuf);
 
 #endif /* !__ARTWORK_H__ */
