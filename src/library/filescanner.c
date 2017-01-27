donfanning/forked-daapd

[cache/scan] Let notifications from db.c about library updates go through filescanner/listener instead of directly to the cache

src/filescanner.c
@@ -61,6 +61,7 @@
 #include "player.h"
 #include "cache.h"
 #include "artwork.h"
+#include "listener.h"
 #include "commands.h"
 
 #ifdef LASTFM
@@ -108,10 +109,17 @@ static int scan_exit;
 static int inofd;
 static struct event_base *evbase_scan;
 static struct event *inoev;
+static struct event *updateev;
 static pthread_t tid_scan;
 static struct deferred_pl *playlists;
 static struct stacked_dir *dirstack;
 static struct commands_base *cmdbase;
+
+// After being told by db that the library was updated through update_trigger(),
+// wait 60 seconds before notifying listeners of LISTENER_DATABASE. This is to
+// avoid bombarding the listeners while there are many db updates, and to make
+// sure they only get a single update (useful for the cache).
+static struct timeval library_update_wait = { 60, 0 };
 
 #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 struct deferred_file
@@ -1241,6 +1249,8 @@ bulk_scan(int flags)
       db_hook_post_scan();
     }
 
+  listener_notify(LISTENER_DATABASE);
+
   // Set scan in progress flag to FALSE
   scanning = 0;
 }
@@ -1921,6 +1931,21 @@ inofd_event_unset(void)
 
 /* Thread: scan */
 
+static void
+update_trigger_cb(int fd, short what, void *arg)
+{
+  listener_notify(LISTENER_DATABASE);
+}
+
+static enum command_state
+update_trigger(void *arg, int *retval)
+{
+  evtimer_add(updateev, &library_update_wait);
+
+  *retval = 0;
+  return COMMAND_END;
+}
+
 static enum command_state
 filescanner_initscan(void *arg, int *retval)
 {
@@ -1951,6 +1976,16 @@ filescanner_fullrescan(void *arg, int *retval)
 
   *retval = 0;
   return COMMAND_END;
+}
+
+// TODO Move to abstraction
+void
+library_update_trigger(void)
+{
+  if (scanning)
+    return;
+
+  commands_exec_async(cmdbase, update_trigger, NULL);
 }
 
 void
@@ -2010,6 +2045,13 @@ filescanner_init(void)
       goto ino_fail;
     }
 
+  updateev = evtimer_new(evbase_scan, update_trigger_cb, NULL);
+  if (!updateev)
+    {
+      DPRINTF(E_FATAL, L_SCAN, "Could not create library update event\n");
+      goto updateev_fail;
+    }
+
   cmdbase = commands_base_new(evbase_scan, NULL);
 
   ret = pthread_create(&tid_scan, NULL, filescanner, NULL);
@@ -2030,6 +2072,7 @@ filescanner_init(void)
 
  thread_fail:
   commands_base_free(cmdbase);
+ updateev_fail:
   close(inofd);
  ino_fail:
   event_base_free(evbase_scan);

owntone/owntone-server

src/library/filescanner.c
@@ -61,6 +61,7 @@
 #include "player.h"
 #include "cache.h"
 #include "artwork.h"
+#include "listener.h"
 #include "commands.h"
 #include "library.h"
 
@@ -107,12 +108,18 @@ struct stacked_dir {
 
 static int inofd;
 static struct event *inoev;
+static struct event *updateev;
 static struct deferred_pl *playlists;
 static struct stacked_dir *dirstack;
 
+// After being told by db that the library was updated through update_trigger(),
+// wait 60 seconds before notifying listeners of LISTENER_DATABASE. This is to
+// avoid bombarding the listeners while there are many db updates, and to make
+// sure they only get a single update (useful for the cache).
+static struct timeval library_update_wait = { 60, 0 };
+
 /* From library.c */
 extern struct event_base *evbase_lib;
-
 
 #ifndef __linux__
 struct deferred_file
@@ -841,6 +848,8 @@ bulk_scan(int flags)
     {
       DPRINTF(E_LOG, L_SCAN, "Bulk library scan completed in %.f sec\n", difftime(end, start));
     }
+
+  listener_notify(LISTENER_DATABASE); // TODO Move to library.c
 }
 
 static int
@@ -1469,6 +1478,21 @@ filescanner_initscan()
   return 0;
 }
 
+static void
+update_trigger_cb(int fd, short what, void *arg)
+{
+  listener_notify(LISTENER_DATABASE);
+}
+
+static enum command_state
+update_trigger(void *arg, int *retval)
+{
+  evtimer_add(updateev, &library_update_wait);
+
+  *retval = 0;
+  return COMMAND_END;
+}
+
 static int
 filescanner_rescan()
 {
@@ -1494,6 +1518,16 @@ filescanner_fullrescan()
   return 0;
 }
 
+// TODO Move to abstraction
+void
+library_update_trigger(void)
+{
+  if (scanning)
+    return;
+
+  commands_exec_async(cmdbase, update_trigger, NULL);
+}
+
 /* Thread: main */
 static int
 filescanner_init(void)
@@ -1501,8 +1535,20 @@ filescanner_init(void)
   int ret;
 
   ret = inofd_event_set();
+  if (ret < 0)
+    {
+      return -1;
+    }
 
-  return ret;
+  updateev = evtimer_new(evbase_lib, update_trigger_cb, NULL);
+  if (!updateev)
+    {
+      DPRINTF(E_FATAL, L_SCAN, "Could not create library update event\n");
+      close(inofd);
+      return -1;
+    }
+
+  return 0;
 }
 
 /* Thread: main */