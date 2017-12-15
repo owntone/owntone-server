a31ce79c9c7e0de0f3263a26ea69e2ab75c870b5
chme <christian.meffert@googlemail.com>
2017-12-15 16:55:40 +0100

    [db] Comment for DB_FILES_RATING_MAX

--- src/db.h
+++ src/db.h
@@ -72,6 +72,7 @@ enum query_type {
 #define DB_ADMIN_LASTFM_SESSION_KEY "lastfm_sk"
 #define DB_ADMIN_SPOTIFY_REFRESH_TOKEN "spotify_refresh_token"
 
+/* Max value for media_file_info->rating (valid range is from 0 to 100) */
 #define DB_FILES_RATING_MAX 100
 
 struct query_params {
