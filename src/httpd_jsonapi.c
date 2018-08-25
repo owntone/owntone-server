c6b1208a5b27acb96d91e976946fbfb9ae8ac107
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2018-08-25 11:56:47 +0100

    [jsonapi] add support for listing albums for genres

--- src/httpd_jsonapi.c
+++ src/httpd_jsonapi.c
@@ -234,12 +234,32 @@ playlist_to_json(struct db_playlist_info *dbpli)
   return item;
 }
 
+static json_object *
+genre_to_json(const char* genre, const struct filecount_info* fci)
+{
+  json_object *item;
+
+  if (genre == NULL)
+    {
+      return NULL;
+    }
+
+  item = json_object_new_object();
+  safe_json_add_string(item, "name", genre);
+  json_object_object_add(item, "album_count",  json_object_new_int(fci ? fci->album_count : 0));
+  json_object_object_add(item, "artist_count", json_object_new_int(fci ? fci->artist_count : 0));
+  json_object_object_add(item, "track_count",  json_object_new_int(fci ? fci->count : 0));
+
+  return item;
+}
+
+
 static int
 fetch_tracks(struct query_params *query_params, json_object *items, int *total)
 {
   struct db_media_file_info dbmfi;
   json_object *item;
-  int ret = 0;
+  int ret;
 
   ret = db_query_start(query_params);
   if (ret < 0)
@@ -459,6 +479,53 @@ fetch_playlist(const char *playlist_id)
 
   return playlist;
 }
+
+static int
+fetch_genres(struct query_params *query_params, json_object *items, int *total)
+{
+  json_object *item;
+  int ret;
+  char* genre;
+  char* sort_item;
+  struct filecount_info fci;
+  struct query_params qp;
+
+  ret = db_query_start(query_params);
+  if (ret < 0)
+    goto error;
+
+  while (((ret = db_query_fetch_string_sort(query_params, &genre, &sort_item)) == 0) && (genre))
+    {
+      memset(&fci, 0, sizeof(fci));
+      memset(&qp, 0, sizeof(qp));
+
+      qp.type = Q_COUNT_ITEMS;
+      qp.idx_type = I_NONE;
+      qp.filter = db_mprintf("(f.genre = '%q')", genre);
+      db_query_start(&qp);
+      db_filecount_get(&fci, &qp);
+      free(qp.filter);
+      db_query_end(&qp);
+
+      item = genre_to_json(genre, &fci);
+      if (!item)
+	{
+	  ret = -1;
+	  goto error;
+	}
+
+      json_object_array_add(items, item);
+    }
+
+  if (total)
+    *total = query_params->results;
+
+ error:
+  db_query_end(query_params);
+
+  return ret;
+}
+
 
 static int
 query_params_limit_set(struct query_params *query_params, struct httpd_request *hreq)
@@ -2357,6 +2424,73 @@ jsonapi_reply_library_playlist_tracks(struct httpd_request *hreq)
 }
 
 static int
+jsonapi_reply_library_genres(struct httpd_request *hreq)
+{
+  time_t db_update;
+  struct query_params query_params;
+  const char *param;
+  enum media_kind media_kind;
+  json_object *reply;
+  json_object *items;
+  int total;
+  int ret;
+
+
+  db_update = (time_t) db_admin_getint64(DB_ADMIN_DB_UPDATE);
+  if (db_update && httpd_request_not_modified_since(hreq->req, &db_update))
+    return HTTP_NOTMODIFIED;
+
+  media_kind = 0;
+  param = evhttp_find_header(hreq->query, "media_kind");
+  if (param)
+    {
+      media_kind = db_media_kind_enum(param);
+      if (!media_kind)
+	{
+	  DPRINTF(E_LOG, L_WEB, "Invalid media kind '%s'\n", param);
+	  return HTTP_BADREQUEST;
+	}
+    }
+
+  reply = json_object_new_object();
+  items = json_object_new_array();
+  json_object_object_add(reply, "items", items);
+
+  memset(&query_params, 0, sizeof(struct query_params));
+
+  ret = query_params_limit_set(&query_params, hreq);
+  if (ret < 0)
+    goto error;
+
+  query_params.type = Q_BROWSE_GENRES;
+  query_params.idx_type = I_NONE;
+
+  if (media_kind)
+    query_params.filter = db_mprintf("(f.media_kind = %d)", media_kind);
+
+  ret = fetch_genres(&query_params, items, &total);
+  if (ret < 0)
+    goto error;
+
+  json_object_object_add(reply, "total", json_object_new_int(total));
+  json_object_object_add(reply, "offset", json_object_new_int(query_params.offset));
+  json_object_object_add(reply, "limit", json_object_new_int(query_params.limit));
+
+  ret = evbuffer_add_printf(hreq->reply, "%s", json_object_to_json_string(reply));
+  if (ret < 0)
+    DPRINTF(E_LOG, L_WEB, "browse: Couldn't add genres to response buffer.\n");
+
+ error:
+  jparse_free(reply);
+  free_query_params(&query_params, 1);
+
+  if (ret < 0)
+    return HTTP_INTERNAL;
+
+  return HTTP_OK;
+}
+
+static int
 jsonapi_reply_library_count(struct httpd_request *hreq)
 {
   time_t db_update;
@@ -2637,6 +2771,66 @@ search_playlists(json_object *reply, struct httpd_request *hreq, const char *par
 }
 
 static int
+search_genres(json_object *reply, struct httpd_request *hreq, const char *param_query, struct smartpl *smartpl_expression, enum media_kind media_kind)
+{
+  json_object *type;
+  json_object *items;
+  struct query_params query_params;
+  int total;
+  int ret;
+
+  memset(&query_params, 0, sizeof(struct query_params));
+
+  ret = query_params_limit_set(&query_params, hreq);
+  if (ret < 0)
+    goto out;
+
+  type = json_object_new_object();
+  json_object_object_add(reply, "genres", type);
+  items = json_object_new_array();
+  json_object_object_add(type, "items", items);
+
+  query_params.type = Q_BROWSE_GENRES;
+  query_params.sort = S_GENRE;
+  query_params.idx_type = I_NONE;
+
+  if (param_query)
+    {
+      if (media_kind)
+	query_params.filter = db_mprintf("(f.genre LIKE '%%%q%%' AND f.media_kind = %d)", param_query, media_kind);
+      else
+	query_params.filter = db_mprintf("(f.genre LIKE '%%%q%%')", param_query);
+    }
+  else
+    {
+      query_params.filter = strdup(smartpl_expression->query_where);
+      query_params.having = safe_strdup(smartpl_expression->having);
+      query_params.order = safe_strdup(smartpl_expression->order);
+
+      if (smartpl_expression->limit > 0)
+	{
+	  query_params.idx_type = I_SUB;
+	  query_params.limit = smartpl_expression->limit;
+	  query_params.offset = 0;
+	}
+    }
+
+  ret = fetch_genres(&query_params, items, &total);
+  if (ret < 0)
+    goto out;
+
+  json_object_object_add(type, "total", json_object_new_int(total));
+  json_object_object_add(type, "offset", json_object_new_int(query_params.offset));
+  json_object_object_add(type, "limit", json_object_new_int(query_params.limit));
+
+ out:
+  free_query_params(&query_params, 1);
+
+  return ret;
+}
+
+
+static int
 jsonapi_reply_search(struct httpd_request *hreq)
 {
   const char *param_type;
@@ -2679,6 +2873,22 @@ jsonapi_reply_search(struct httpd_request *hreq)
   if (param_expression)
     {
       expression = safe_asprintf("\"query\" { %s }", param_expression);
+
+#ifndef ANTLR_PARSER_FIX
+      /* HACK -- a bug in smartpl_query.c:parse_input() causes svr to lockup 
+       * when it doesn't get a string it doesn't like (the %2B instead of +):
+       * ie /api/search?type=albums&media_type=music&expression=genre%2Bis%2B%22Pop%22
+       * causes 'expression' with '+' instead of ' '
+       */
+      {
+          DPRINTF(E_DBG, L_WEB, " in smart expr= '%s'\n", expression);
+          char*  p;
+          while ( (p = strchr(expression, '+')) ) {
+              *p = ' ';
+          }
+          DPRINTF(E_DBG, L_WEB, "out smart expr= '%s'\n", expression);
+      }
+#endif
       ret = smartpl_query_parse_string(&smartpl_expression, expression);
       free(expression);
 
@@ -2715,6 +2925,14 @@ jsonapi_reply_search(struct httpd_request *hreq)
       if (ret < 0)
 	goto error;
     }
+
+ if (strstr(param_type, "genre"))
+    {
+      ret = search_genres(reply, hreq, param_query, &smartpl_expression, media_kind);
+      if (ret < 0)
+	goto error;
+    }
+
 
   ret = evbuffer_add_printf(hreq->reply, "%s", json_object_to_json_string(reply));
   if (ret < 0)
@@ -2779,6 +2997,7 @@ static struct httpd_uri_map adm_handlers[] =
     { EVHTTP_REQ_GET,    "^/api/library/albums$",                        jsonapi_reply_library_albums },
     { EVHTTP_REQ_GET,    "^/api/library/albums/[[:digit:]]+$",           jsonapi_reply_library_album },
     { EVHTTP_REQ_GET,    "^/api/library/albums/[[:digit:]]+/tracks$",    jsonapi_reply_library_album_tracks },
+    { EVHTTP_REQ_GET,    "^/api/library/genres$",                        jsonapi_reply_library_genres},
     { EVHTTP_REQ_GET,    "^/api/library/count$",                         jsonapi_reply_library_count },
 
     { EVHTTP_REQ_GET,    "^/api/search$",                                jsonapi_reply_search },
