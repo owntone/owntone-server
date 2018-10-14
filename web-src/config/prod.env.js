e4f288c5b48cd8f6d9d8da7317a102e7f2f14f6c
chme <christian.meffert@googlemail.com>
2018-10-14 10:47:54 +0200

    [web-src] Change player web interface build system to use vue-cli v3
    Also bumps the player web interface version to 0.2.0.
    
    Previously the build was based on the vue.js webpack template. The
    template is deprecated and the recommended method for setting up a vue
    application is vue-cli. This also changes the build from using webpack
    v3 to webpack v4.

--- web-src/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  VERSION: '"123"',
-  V2: JSON.stringify(require("../package.json").version)
-}
---
+++ web-src/vue.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // Runtime compiler is required to compile vue templates
+  runtimeCompiler: true,
+
+  // Output path for the generated static assets (js/css)
+  outputDir: '../htdocs',
+
+  // Output path for the generated index.html
+  indexPath: 'index.html',
+  
+  assetsDir: 'player',
+
+  // Do not add hashes to the generated js/css filenames, would otherwise
+  // require to adjust the Makefile in htdocs each time the web interface is
+  // build
+  filenameHashing: false,
+
+  css: {
+    sourceMap: true
+  },
+
+  devServer: {
+    // Proxy forked-daapd JSON API calls to the forked-daapd server running on
+    // localhost:3689
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3689',
+      }
+    }
+  }
+}
