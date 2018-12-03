4f6c9bb366fc027c53208385ec683d8931c90cd6
ejurgensen <espenjurgensen@gmail.com>
2018-12-03 23:59:31 +0100

    Revert "[player] Do not abort if pausing a stream or pipe playback"
    This reverts commit fd84dd51b524f672fee188bc718a557ffa60ff66.

--- src/player.c
+++ src/player.c
@@ -2003,19 +2003,6 @@ playback_start_item(void *arg, int *retval)
       // Resume playback of current source
       ps = source_now_playing();
       DPRINTF(E_DBG, L_PLAYER, "Resume playback of '%s' (id=%d, item-id=%d)\n", ps->path, ps->id, ps->item_id);
-
-      // Check if source needs to be reopend
-      if (!ps->setup_done)
-        {
-	  DPRINTF(E_INFO, L_PLAYER, "Opening '%s'\n", ps->path);
-
-	  ret = input_setup(ps);
-	  if (ret < 0)
-	    {
-	      DPRINTF(E_LOG, L_PLAYER, "Failed to open '%s'\n", ps->path);
-	      return -1;
-	    }
-	}
     }
   else
     {
@@ -2343,11 +2330,10 @@ playback_pause_bh(void *arg, int *retval)
 
   if (cur_streaming->data_kind == DATA_KIND_HTTP || cur_streaming->data_kind == DATA_KIND_PIPE)
     {
-      // For stream and pipe input we stop reading from the source but still switch to the paused state.
-      // (Resuming playback will reopen the source)
-      DPRINTF(E_DBG, L_PLAYER, "Source is not pausable, stop playback\n");
+      DPRINTF(E_DBG, L_PLAYER, "Source is not pausable, abort playback\n");
 
-      input_stop(cur_streaming);
+      playback_abort();
+      return COMMAND_END;
     }
   status_update(PLAY_PAUSED);
 
