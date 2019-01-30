7abf68200de65f89d6d5481a9423b42195630f1d
ejurgensen <espenjurgensen@gmail.com>
2019-01-30 22:32:46 +0100

    [scan] Fix parsing of iTunes "Play Date" tag (fixes #683)
    "Play Date" tag was seconds since 1904 (an Apple Mac HFS+ timestamp), not a
    Unix timestamp as we assumed. Seems Apple themselves realised that wasn't a
    great idea (+ not a proper plist date type), and therefore provide "Play Date
    UTC" as an alternative.

--- src/library/filescanner_itunes.c
+++ src/library/filescanner_itunes.c
@@ -126,7 +126,7 @@ static struct metadata_map md_map[] =
     { "Rating",       PLIST_UINT,    mfi_offsetof(rating) },
     { "Compilation",  PLIST_BOOLEAN, mfi_offsetof(compilation) },
     { "Date Added",   PLIST_DATE,    mfi_offsetof(time_added) },
-    { "Play Date",    PLIST_UINT,    mfi_offsetof(time_played) },
+    { "Play Date UTC",PLIST_DATE,    mfi_offsetof(time_played) },
     { "Play Count",   PLIST_UINT,    mfi_offsetof(play_count) },
     { "Skip Count",   PLIST_UINT,    mfi_offsetof(skip_count) },
     { "Skip Date",    PLIST_DATE,    mfi_offsetof(time_skipped) },
