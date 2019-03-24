95fded0afddb9ed4c2e22b11f02350313cf3dfca
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-03-24 15:08:00 +0000

    [db] add album_artist_sort for album sorting - handles listing for album names are the same but different artists

--- src/db.c
+++ src/db.c
@@ -395,7 +395,7 @@ static const char *sort_clause[] =
   {
     "",
     "f.title_sort",
-    "f.album_sort, f.disc, f.track, f.title_sort",
+    "f.album_sort, f.album_artist_sort, f.disc, f.track, f.title_sort",
     "f.album_artist_sort, f.album_sort, f.disc, f.track, f.title_sort",
     "f.type, f.parent_id, f.special_id, f.title",
     "f.year",
