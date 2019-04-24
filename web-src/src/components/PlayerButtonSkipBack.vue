cb6b1919a8d6aa64fc4f58b4738871c0ab0dc90b
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-04-24 23:02:51 +0100

    [web-src] add fast skip fwd/back on now playing
    Conflicts:
    	web-src/src/pages/PageNowPlaying.vue

---
+++ web-src/src/components/PlayerButtonSkipBack.vue
@@ -0,0 +1,25 @@
+<template>
+  <a v-on:click="play_skip_back">
+    <span class="icon"><i class="mdi mdi-replay"></i></span>
+  </a>
+</template>
+
+<script>
+import webapi from '@/webapi'
+
+export default {
+  name: 'PlayerButtonSkipBack',
+  props: [ 'when_ms' ],
+
+  methods: {
+    play_skip_back: function () {
+      if (this.$store.state.player.state !== 'stop') {
+        webapi.player_seek(this.when_ms - 10000)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
