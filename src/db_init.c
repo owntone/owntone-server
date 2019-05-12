d56215f4e6ec85e6fb531a54df6231e7e0c503a3
ejurgensen <espenjurgensen@gmail.com>
2019-05-12 23:28:38 +0200

    [db] Move calculation of artist/album id's from sqlite to code
    The purpose of this is to support library backends making their own
    calculation of these id's, which is relevant if they have more information
    available than just album_artist and album.
    
    This also removes a bunch of sqlite extension code plus some triggers, which
    in itself is probably an improvement.

--- src/db_init.c
+++ src/db_init.c
@@ -400,22 +400,6 @@ static const struct db_init_query db_init_index_queries[] =
 
 /* Triggers must be prefixed with trg_ for db_drop_triggers() to id them */
 
-#define TRG_FILES_INSERT_SONGIDS									\
-  "CREATE TRIGGER trg_files_insert_songids AFTER INSERT ON files FOR EACH ROW"				\
-  " BEGIN"												\
-  "   UPDATE files SET songartistid = daap_songalbumid(LOWER(NEW.album_artist), ''), "			\
-  "     songalbumid = daap_songalbumid(LOWER(NEW.album_artist), LOWER(NEW.album))"			\
-  "   WHERE id = NEW.id;"										\
-  " END;"
-
-#define TRG_FILES_UPDATE_SONGIDS									\
-  "CREATE TRIGGER trg_files_update_songids AFTER UPDATE OF album_artist, album ON files FOR EACH ROW"	\
-  " BEGIN"												\
-  "   UPDATE files SET songartistid = daap_songalbumid(LOWER(NEW.album_artist), ''), "			\
-  "     songalbumid = daap_songalbumid(LOWER(NEW.album_artist), LOWER(NEW.album))"			\
-  "   WHERE id = NEW.id;"										\
-  " END;"
-
 #define TRG_GROUPS_UPDATE										\
   "CREATE TRIGGER trg_groups_update AFTER UPDATE OF songartistid, songalbumid ON files FOR EACH ROW"	\
   " WHEN (NEW.songartistid != 0 AND NEW.songalbumid != 0)"						\
@@ -426,8 +410,6 @@ static const struct db_init_query db_init_index_queries[] =
 
 static const struct db_init_query db_init_trigger_queries[] =
   {
-    { TRG_FILES_INSERT_SONGIDS,    "create trigger trg_files_insert_songids" },
-    { TRG_FILES_UPDATE_SONGIDS,    "create trigger trg_files_update_songids" },
     { TRG_GROUPS_UPDATE,           "create trigger trg_groups_update" },
   };
 
