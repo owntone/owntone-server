25ee55a9600d4980f17d0c8f845088a7bff07025
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2020-02-14 09:11:54 +0000

    [conf] ignore png/jpg files

--- src/conffile.c
+++ src/conffile.c
@@ -92,7 +92,7 @@ static cfg_opt_t sec_library[] =
     CFG_STR("name_radio", "Radio", CFGF_NONE),
     CFG_STR_LIST("artwork_basenames", "{artwork,cover,Folder}", CFGF_NONE),
     CFG_BOOL("artwork_individual", cfg_false, CFGF_NONE),
-    CFG_STR_LIST("filetypes_ignore", "{.db,.ini,.db-journal,.pdf,.metadata}", CFGF_NONE),
+    CFG_STR_LIST("filetypes_ignore", "{.db,.ini,.db-journal,.pdf,.metadata,.png,.jpg}", CFGF_NONE),
     CFG_STR_LIST("filepath_ignore", NULL, CFGF_NONE),
     CFG_BOOL("filescan_disable", cfg_false, CFGF_NONE),
     CFG_BOOL("m3u_overrides", cfg_false, CFGF_NONE),
