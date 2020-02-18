31289e44805feee3b888d739df3cbe9f4cb87ec7
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2020-02-18 19:48:08 +0000

    [misc] rss helper methods

---
+++ src/misc_rss.c
@@ -0,0 +1,596 @@
+/*
+ * Copyright (C) 2020 whatdoineed2d/Ray
+ * based heavily on filescanner_playlist.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "misc_rss.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE
+#endif
+#include <time.h>
+
+#include <event2/event.h>
+#include <mrss.h>
+
+#include "logger.h"
+#include "db.h"
+#include "http.h"
+#include "misc.h"
+#include "misc_json.h"
+#include "library.h"
+#include "library/filescanner.h"
+
+
+void
+free_rfi(struct rss_file_item* rfi)
+{
+  if (!rfi) return;
+
+  struct rss_file_item *tmp;
+  while (rfi)
+  {
+    tmp = rfi->next;
+    free(rfi->url);
+    free(rfi->title);
+    free(rfi);
+    rfi = tmp;
+  }
+}
+
+// should only be called by rfi_add() and if you are getting a new list
+struct rss_file_item*
+rfi_alloc()
+{
+  struct rss_file_item *obj = malloc(sizeof(struct rss_file_item));
+  memset(obj, 0, sizeof(struct rss_file_item));
+  return obj;
+}
+
+// returns the newly alloc'd / added item at end of list
+struct rss_file_item*
+rfi_add(struct rss_file_item* head)
+{
+  struct rss_file_item *curr = head;
+  while (curr->next)
+    curr = curr->next;
+
+  curr->next = rfi_alloc();
+  return curr->next;
+}
+
+
+static int
+rss_playlist_fill(struct playlist_info *pli, const char *path, const char *name)
+{
+  memset(pli, 0, sizeof(struct playlist_info));
+
+  pli->type  = PL_RSS;
+  pli->path  = strdup(path);
+  pli->title = strdup(name);
+  pli->virtual_path = malloc(strlen(path)+2);
+  sprintf(pli->virtual_path, "/%s", path);
+
+  return 0;
+}
+
+static int
+rss_playlist_add(const char *path, const char *name)
+{
+  struct playlist_info pli;
+  int ret;
+
+  ret = rss_playlist_fill(&pli, path, name);
+  if (ret < 0)
+    return -1;
+
+  ret = library_playlist_save(&pli);
+  if (ret < 0)
+    {
+      free_pli(&pli, 1);
+      return -1;
+    }
+
+  free_pli(&pli, 1);
+
+  return ret;
+}
+
+static int
+rss_playlist_prepare(const char *path, const char *name, time_t mtime, bool *isnew)
+{
+  struct playlist_info *pli;
+  int pl_id;
+
+  pli = db_pl_fetch_bypath(path);
+  if (!pli)
+    {
+      DPRINTF(E_LOG, L_RSS, "New RSS found, processing '%s'\n", path);
+      *isnew = true;
+
+      pl_id = rss_playlist_add(path, name);
+      if (pl_id < 0)
+        {
+          DPRINTF(E_LOG, L_RSS, "Error adding RSS '%s'\n", path);
+          return -1;
+        }
+
+      DPRINTF(E_INFO, L_RSS, "Added new RSS as id %d\n", pl_id);
+      return pl_id;
+    }
+  *isnew = false;
+
+  pl_id = pli->id;
+  free_pli(pli, 0);
+
+  return pl_id;
+}
+
+// RSS spec: https://validator.w3.org/feed/docs/rss2.html
+
+static bool
+rss_date(struct tm *tm, const char *date)
+{
+  // RFC822 https://tools.ietf.org/html/rfc822#section-5
+  // ie Fri, 07 Feb 2020 18:58:00 +0000
+  //    ^^^^                      ^^^^^
+  //    optional                  ^^^^^
+  //                              could also be GMT/UT/EST/A..I/M..Z
+
+  char *ptr;
+  time_t t;
+
+  memset(tm, 0, sizeof(struct tm));
+  ptr = strptime(date, "%a,%n", tm);
+  ptr = strptime(ptr ? ptr : date, "%d%n%b%n%Y%n%H:%M:%S%n", tm);
+  if (!ptr)
+    {
+      // date is junk, using current time
+      time(&t);
+      gmtime_r(&t, tm);
+      return false;
+    }
+
+  // TODO - adjust the TZ?
+  return true;
+}
+
+// uses incoming buf for result but if too smal, returns new buf
+static char*
+process_apple_rss(const char *rss_url)
+{
+  struct http_client_ctx ctx;
+  struct evbuffer *evbuf;
+  char url[100];
+  char *buf = NULL;
+  unsigned podid;  // apple podcast id
+  json_object *json = NULL;
+  json_object *jsonra = NULL;
+  const char *feedURL;
+  const char *ptr;
+  int ret;
+
+  // ask for the json to get feedUrl
+  // https://itunes.apple.com/lookup?id=974722423
+
+  ptr = strrchr(rss_url, '/');
+  if (!ptr)
+    {
+      DPRINTF(E_LOG, L_RSS, "Could not parse Apple Podcast RSS ID from '%s'\n", rss_url);
+      return NULL;
+    }
+  if (sscanf(ptr, "/id%u", &podid) != 1)
+    {
+      DPRINTF(E_LOG, L_RSS, "Could not parse Apple Podcast RSS ID from '%s'\n", rss_url);
+      return NULL;
+    }
+
+  evbuf = evbuffer_new();
+  if (!evbuf)
+    return false;
+
+  snprintf(url, sizeof(url), "https://itunes.apple.com/lookup?id=%u", podid);
+
+  memset(&ctx, 0, sizeof(struct http_client_ctx));
+  ctx.url = url;
+  ctx.input_body = evbuf;
+
+  ret = http_client_request(&ctx);
+  if (ret < 0 || (ret && ctx.response_code != HTTP_OK))
+    {
+      evbuffer_free(evbuf);
+      return NULL;
+    }
+
+  json = jparse_obj_from_evbuffer(evbuf);
+  if (!json)
+    {
+      DPRINTF(E_LOG, L_RSS, "Could not parse RSS apple response, podcast id %u\n", podid);
+    }
+  else
+    {
+      /* expect json resp - get feedUrl
+       * {
+       *  "resultCount": 1,
+       *  "results": [
+       *    {
+       *      "wrapperType": "track",
+       *      "kind": "podcast",
+       *      ...
+       *      "collectionViewUrl": "https://podcasts.apple.com/us/podcast/cgp-grey/id974722423?uo=4",
+       *      "feedUrl": "http://cgpgrey.libsyn.com/rss",
+       *      ...
+       *      "genres": [
+       *        "Education",
+       *        "Podcasts",
+       *        "News"
+       *      ]
+       *    }
+       *  ]
+       *}
+       */
+      if (json_object_object_get_ex(json, "results", &jsonra) && (feedURL = jparse_str_from_array(jsonra, 0, "feedUrl")) )
+        {
+          buf = strcpy(malloc(strlen(feedURL)+1), feedURL);
+        }
+      else
+        DPRINTF(E_DBG, L_RSS, "Could not parse feedURL from RSS apple, podcast id %u\n", podid);
+    }
+
+  jparse_free(json);
+  evbuffer_free(evbuf);
+  return buf;
+}
+
+static bool
+process_image_url(const char *image_url, const char *file)
+{
+  char *ptr;
+  const char *img_extn;
+  char path[PATH_MAX];
+
+  struct http_client_ctx ctx;
+  struct evbuffer *evbuf;
+  int fd;
+  int ret;
+
+  if (!image_url || !file)
+    return false;
+
+  if ((img_extn = strrchr(image_url, '.')) == NULL)
+    return false;
+ 
+  strncpy(path, file, sizeof(path));
+  if ( (ptr = strrchr(path, '.')) == NULL)
+    return false;
+
+  strcpy(ptr, img_extn);
+
+  if ((fd = open(path, O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0)
+    {
+      DPRINTF(E_INFO, L_RSS, "Could not create file '%s' for RSS image: %s\n", path, strerror(errno));
+      return false;
+    }
+
+  evbuf = evbuffer_new();
+  if (!evbuf)
+    {
+      close(fd);
+      return false;
+    }
+
+  memset(&ctx, 0, sizeof(struct http_client_ctx));
+  ctx.url = image_url;
+  ctx.input_body = evbuf;
+
+  ret = http_client_request(&ctx);
+  if (ret < 0 || (ret && ctx.response_code != HTTP_OK))
+    {
+      DPRINTF(E_INFO, L_RSS, "Could not retreive RSS image\n");
+    }
+  else
+    {
+      while (evbuffer_write(evbuf, fd) > 0) ;
+    }
+
+  close(fd);
+  evbuffer_free(evbuf);
+
+  return true;
+}
+
+
+#ifdef RSS_DEBUG
+static void
+rss_playlist_items(int plid)
+{
+  struct query_params qp;
+  struct db_media_file_info dbpli;
+  int ret;
+
+  memset(&qp, 0, sizeof(struct query_params));
+
+  qp.type = Q_PLITEMS;
+  qp.idx_type = I_NONE;
+  qp.id = plid;
+
+  ret = db_query_start(&qp);
+  if (ret < 0)
+    {
+      db_query_end(&qp);
+      return;
+    }
+  while (((ret = db_query_fetch_file(&qp, &dbpli)) == 0) && (dbpli.id))
+    {
+      DPRINTF(E_LOG, L_RSS, "plid=%u  { id=%s title=%s path=%s }\n", plid, dbpli.id, dbpli.title, dbpli.path);
+    }
+  db_query_end(&qp);
+
+  return;
+}
+#endif
+
+
+int
+rss_feed_refresh(int pl_id, time_t mtime, const char *url, unsigned *nadded)
+{
+  struct media_file_info mfi;
+  char *vpath = NULL;
+  int feed_file_id;
+  struct tm tm;
+  unsigned vpathlen = 0;
+  unsigned len = 0;
+  bool has_artwork = false;
+
+  const char *category_author = NULL;
+  mrss_t *data = NULL;
+  mrss_tag_t *tag = NULL;
+  mrss_error_t mret;
+  mrss_item_t *item = NULL;
+  CURLcode code;
+
+  int ret = 0;
+
+  DPRINTF(E_DBG, L_RSS, "Refreshing RSS id: %u url:%s\n", pl_id, url);
+  db_pl_ping(pl_id);
+  db_pl_ping_items_bymatch("http://", pl_id);
+  db_pl_ping_items_bymatch("https://", pl_id);
+
+  mret = mrss_parse_url_with_options_and_error((char*)url, &data, NULL, &code);
+  if (mret)
+    {
+      DPRINTF(E_LOG, L_RSS, "Could not parse RSS from '%s': %s\n", url, mret==MRSS_ERR_DOWNLOAD ? mrss_curl_strerror(code) : mrss_strerror(mret));
+      ret = -1;
+      goto cleanup;
+    }
+
+  /* Expect: 'Channel' (used as 'album name') followed by sequence of 'Items' (used as 'tracks' with artist,title,enclosure_url)
+   * Look at pubDate in items to determine if it's new
+   *
+        Channel:
+                title: CGP Grey
+                description: CGP Grey explains videos.
+                link: http://www.cgpgrey.com
+                language: en
+                rating: (null)
+                copyright: 2011-2015
+                pubDate: Sun, 26 Jan 2020 15:43:07 +0000
+                lastBuildDate: Sun, 09 Feb 2020 15:48:27 +0000
+                docs: http://www.cgpgrey.com
+                managingeditor: (null)
+                webMaster: (null)
+                generator: Libsyn WebEngine 2.0
+                ttl: 0
+        Image:
+                image_title: CGP Grey
+                image_url: http://static.libsyn.com/p/assets/0/a/6/3/0a636b838b034e38/cgp-logo-itunes.png
+                image_link: http://www.cgpgrey.com
+                image_width: 0
+                image_height: 0
+                image_description: (null)
+        Items:
+                title: Your Theme
+                link: http://cgpgrey.libsyn.com/your-theme
+                description:
+                author: (null)
+                comments: (null)
+                pubDate: Sun, 26 Jan 2020 15:43:07 +0000
+                guid: 8d394457-02b5-4bc8-bce8-619aa87d395c
+                guid_isPermaLink: 0
+                source: (null)
+                source_url: (null)
+                enclosure: (null)
+                enclosure_url: http://traffic.libsyn.com/cgpgrey/Your_New_Years_Resolution_Has_Already_Failed.mp4?dest-id=256939
+                enclosure_length: 25467531
+                enclosure_type: video/mp4
+   */
+
+
+  // Author can be found in the following places in order of preferance:
+  // .. the media stream object itself (but not m4v/mp4 streams)
+  // .. feed's item object
+  // .. channel's author tag (failsafe)
+  tag = data->other_tags;
+  while (tag)
+    {
+      if (strcmp(tag->name, "author") == 0)
+        {
+          category_author = tag->value;
+          break;
+        }
+      tag = tag->next;
+    }
+
+  item = data->item;
+  while (item)
+    {
+      DPRINTF(E_DBG, L_RSS, "Feed provides RSS id: %d name: '%s' url: %s pubdate: %s title: '%s'\n", pl_id, data->title, item->enclosure_url, item->pubDate, item->title);
+      if (item->enclosure_url)
+        {
+	  memset(&mfi, 0, sizeof(struct media_file_info));
+
+	  len = strlen(item->enclosure_url)+2;
+	  if (len > vpathlen)
+	    {
+	      vpathlen = len;
+	      free(vpath);
+	      vpath = malloc(len);
+	    }
+	  sprintf(vpath, "/%s", item->enclosure_url);
+
+	  // check if this item is already in the db - if so, we can stop since the RSS is given to us as LIFO stream
+	  if ((feed_file_id = db_file_id_by_virtualpath_match(vpath)) > 0)
+	    {
+	      DPRINTF(E_DBG, L_RSS, "Most recent DB RSS id: %d name: '%s' url: %s file_id: %d pubdate: %s title: '%s'\n", pl_id, data->title, url, feed_file_id, item->pubDate, item->title);
+	      break;
+	    }
+	  DPRINTF(E_INFO, L_RSS, "Adding item to RSS id: %d name: '%s' url: %s pubdate: %s title: '%s'\n", pl_id, data->title, item->enclosure_url, item->pubDate, item->title);
+
+	  // attempt to get artwork if its this is first new item in feed
+	  if (data->image_url && *nadded == 0)
+	    has_artwork = process_image_url(data->image_url, url);  // TODO!!! no local file path to download
+
+          scan_metadata_stream(&mfi, item->enclosure_url);
+
+          // Always take the meta from media file if possible; some podcasts
+          // (apple) can use mp4 streams which tend not to have decent tags so 
+          // in those cases take info from the RSS and not the stream
+          if (!mfi.artist) mfi.artist = safe_strdup(item->author ? item->author : category_author);
+          if (!mfi.album)  mfi.album  = safe_strdup(data->title);
+          if (!mfi.url)    mfi.url    = safe_strdup(item->link);
+          if (!mfi.genre)  mfi.genre  = strdup("Podcast");
+
+          // Title not valid on mp4 (it becomes the url obj) so take from RSS feed
+          free(mfi.title); mfi.title  = safe_strdup(item->title);
+
+          // Ignore this - some can be very verbose - we don't show use these
+          // on the podcast
+          free(mfi.comment); mfi.comment = NULL;
+
+          // date is always from the RSS feed info
+          rss_date(&tm, item->pubDate);
+          mfi.date_released = mktime(&tm);
+          mfi.year = 1900 + tm.tm_year;
+
+          mfi.media_kind = MEDIA_KIND_PODCAST;
+
+	  // Fake the time - useful when we are adding a new stream - since the
+	  // newest podcasts are added first (the stream is most recent first)
+	  // having time_added date which is older on the most recent episodes
+	  // makes no sense so make all the dates the same for a singleu update
+	  mfi.time_added = mtime;
+
+          if (has_artwork)
+            mfi.artwork = ARTWORK_DIR;
+
+          mfi.id = db_file_id_bypath(item->enclosure_url);
+
+          ret = library_media_save(&mfi);
+          db_pl_add_item_bypath(pl_id, item->enclosure_url);
+
+          *nadded = *nadded +1;
+          if (*nadded%50 == 0)
+            {
+              DPRINTF(E_INFO, L_RSS, "RSS added %d entries...\n", *nadded);
+            }
+        }
+      free_mfi(&mfi, 1);
+      item = item->next;
+    }
+
+cleanup:
+  mrss_free(data);
+  free(vpath);
+
+  return ret;
+}
+
+int
+rss_add(const char *name, const char *feed_url)
+{
+  int pl_id = -1;
+  char *apple_url = NULL;
+  const char *url = NULL;
+  time_t now;
+  unsigned nadded = 0;
+  bool isnew = false;
+  int ret;
+
+  DPRINTF(E_DBG, L_RSS, "RSS working on: '%s' '%s'\n", name, feed_url);
+  if (strncmp(feed_url, "http://", 7) != 0 && strncmp(feed_url, "https://", 8) != 0)
+    {
+      DPRINTF(E_LOG, L_RSS, "Invalid RSS url '%s'\n", feed_url);
+      return -1;
+    }
+
+  time(&now);
+  pl_id = rss_playlist_prepare(feed_url, name, now, &isnew);
+  if (pl_id < 0 || !isnew)
+    return -1;
+
+  url = feed_url;
+
+  // Is it an apple podcast stream? ie https://podcasts.apple.com/is/podcast/cgp-grey/id974722423
+  if (strncmp(feed_url, "https://podcasts.apple.com/", 27) == 0)
+    url = apple_url = process_apple_rss(feed_url);
+
+  ret = rss_feed_refresh(pl_id, now, url, &nadded);
+  free(apple_url);
+  if (ret < 0) 
+    {
+      DPRINTF(E_LOG, L_RSS, "Failed to add RSS %s\n", feed_url);
+      db_pl_delete(pl_id);
+      return -1;
+    }
+
+  DPRINTF(E_LOG, L_RSS, "Done processing RSS %s added/modified %u items\n", feed_url, nadded);
+
+  return 0;
+}
+
+int
+rss_remove(const char *name, const char *feed_url)
+{
+  int pl_id;
+
+  DPRINTF(E_DBG, L_RSS, "removing RSS: '%s' '%s'\n", name, feed_url);
+  pl_id = db_pl_id_bypath(feed_url);
+  if (pl_id < 0)
+    {
+      DPRINTF(E_INFO, L_RSS, "Cannot remove RSS - No such RSS feed: '%s' '%s'\n", name, feed_url);
+      return -1;
+    }
+
+  db_pl_clear_items(pl_id);
+  db_pl_delete(pl_id);
+
+  return 0;
+}
---
+++ src/misc_rss.h
@@ -0,0 +1,36 @@
+#ifndef MISC_RSS_H
+#define MISC_RSS_H
+
+#include <time.h>
+
+// relevant fields from playlist tbl
+struct rss_file_item {
+  int id;
+  char *title;
+  char *url;
+  time_t lastupd;
+
+  struct rss_file_item *next;
+};
+
+
+struct rss_file_item*
+rfi_alloc();
+
+struct rss_file_item*
+rfi_add(struct rss_file_item* head);
+
+void
+free_rfi(struct rss_file_item* rfi);
+
+
+int
+rss_add(const char *name, const char *url);
+
+int
+rss_remove(const char *name, const char *url);
+
+int
+rss_feed_refresh(int pl_id, time_t mtime, const char *url, unsigned *nadded);
+
+#endif
