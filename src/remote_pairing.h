accab800ac5e20b433f91863c2ea89d7be64af02
chme <christian.meffert@googlemail.com>
2020-03-15 11:28:21 +0100

    [pairing] Refactor remote_pairing to use cmdbase
    This allows to call a sync command that returns the outcome of a pairing
    request. And that will be useful for the JSON API and the web interface.

--- src/remote_pairing.h
+++ src/remote_pairing.h
@@ -2,8 +2,14 @@
 #ifndef __REMOTE_PAIRING_H__
 #define __REMOTE_PAIRING_H__
 
+#define REMOTE_ERROR -1
+#define REMOTE_INVALID_PIN -2
+
 void
 remote_pairing_kickoff(char **arglist);
+
+int
+remote_pairing_pair(const char *pin);
 
 char *
 remote_pairing_get_name(void);
