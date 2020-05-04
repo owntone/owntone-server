778d6f4999c3fe5a96765e4b64c137fa5491abba
ejurgensen <espenjurgensen@gmail.com>
2020-05-04 17:41:57 +0200

    [config] Change speaker_autoselect default to no
    The changes in commit 6612899 mean that we now do more to maintain selection of
    a user selected speaker, even if it fails or is powered off for a while. Thus
    this option is no longer relevant.
    
    The option was also never very good for people with many speakers.

--- src/conffile.c
+++ src/conffile.c
@@ -54,7 +54,7 @@ static cfg_opt_t sec_general[] =
     CFG_BOOL("ipv6", cfg_true, CFGF_NONE),
     CFG_STR("cache_path", STATEDIR "/cache/" PACKAGE "/cache.db", CFGF_NONE),
     CFG_INT("cache_daap_threshold", 1000, CFGF_NONE),
-    CFG_BOOL("speaker_autoselect", cfg_true, CFGF_NONE),
+    CFG_BOOL("speaker_autoselect", cfg_false, CFGF_NONE),
 #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
     CFG_BOOL("high_resolution_clock", cfg_false, CFGF_NONE),
 #else
