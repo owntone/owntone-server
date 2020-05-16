d2b5e828377d441695982e413d1156daf5cb147f
chme <christian.meffert@googlemail.com>
2020-05-16 06:14:21 +0200

    [web-src] Show album cover artwork in album page

--- web-src/src/components/ListItemAlbum.vue
+++ web-src/src/components/ListItemAlbum.vue
@@ -1,10 +1,13 @@
 <template functional>
   <div class="media" :id="'index_' + props.album.name_sort.charAt(0).toUpperCase()">
+    <slot name="artwork"></slot>
     <div class="media-content fd-has-action is-clipped" @click="listeners.click">
-      <h1 class="title is-6">{{ props.album.name }}</h1>
-      <h2 class="subtitle is-7 has-text-grey"><b>{{ props.album.artist }}</b></h2>
+      <div style="margin-top:0.7rem;">
+        <h1 class="title is-6">{{ props.album.name }}</h1>
+        <h2 class="subtitle is-7 has-text-grey"><b>{{ props.album.artist }}</b></h2>
+      </div>
     </div>
-    <div class="media-right">
+    <div class="media-right" style="padding-top:0.7rem;">
       <slot name="actions"></slot>
     </div>
   </div>
--- web-src/src/pages/PageAlbum.vue
+++ web-src/src/pages/PageAlbum.vue
@@ -1,21 +1,29 @@
 <template>
-  <content-with-heading>
+  <content-with-hero>
     <template slot="heading-left">
-      <div class="title is-4">{{ album.name }}</div>
-      <a class="title is-4 has-text-link has-text-weight-normal" @click="open_artist">{{ album.artist }}</a>
-    </template>
-    <template slot="heading-right">
-      <div class="buttons is-centered">
-        <a class="button is-small is-light is-rounded" @click="show_album_details_modal = true">
-          <span class="icon"><i class="mdi mdi-dots-horizontal mdi-18px"></i></span>
-        </a>
+      <h1 class="title is-4 fd-has-margin-top">{{ album.name }}</h1>
+      <h2 class="subtitle is-4 has-text-link has-text-weight-normal"><a class="has-text-link" @click="open_artist">{{ album.artist }}</a></h2>
+      <p class="heading has-text-centered-mobile">{{ album.track_count }} tracks</p>
+
+      <div class="buttons fd-is-centered-mobile fd-has-margin-top">
         <a class="button is-small is-dark is-rounded" @click="play">
           <span class="icon"><i class="mdi mdi-shuffle"></i></span> <span>Shuffle</span>
         </a>
+        <a class="button is-small is-light is-rounded" @click="show_album_details_modal = true">
+          <span class="icon"><i class="mdi mdi-dots-horizontal mdi-18px"></i></span>
+        </a>
       </div>
     </template>
+    <template slot="heading-right">
+      <p class="image is-square fd-has-shadow">
+        <cover-artwork
+          :artwork_url="album.artwork_url"
+          :artist="album.artist"
+          :album="album.name"
+          class="" />
+      </p>
+    </template>
     <template slot="content">
-      <p class="heading has-text-centered-mobile">{{ album.track_count }} tracks</p>
       <list-item-track v-for="(track, index) in tracks" :key="track.id" :track="track" @click="play_track(index)">
         <template slot="actions">
           <a @click="open_dialog(track)">
@@ -26,15 +34,16 @@
       <modal-dialog-track :show="show_details_modal" :track="selected_track" @close="show_details_modal = false" />
       <modal-dialog-album :show="show_album_details_modal" :album="album" @close="show_album_details_modal = false" />
     </template>
-  </content-with-heading>
+  </content-with-hero>
 </template>
 
 <script>
 import { LoadDataBeforeEnterMixin } from './mixin'
-import ContentWithHeading from '@/templates/ContentWithHeading'
+import ContentWithHero from '@/templates/ContentWithHero'
 import ListItemTrack from '@/components/ListItemTrack'
 import ModalDialogTrack from '@/components/ModalDialogTrack'
 import ModalDialogAlbum from '@/components/ModalDialogAlbum'
+import CoverArtwork from '@/components/CoverArtwork'
 import webapi from '@/webapi'
 
 const albumData = {
@@ -54,7 +63,7 @@ const albumData = {
 export default {
   name: 'PageAlbum',
   mixins: [LoadDataBeforeEnterMixin(albumData)],
-  components: { ContentWithHeading, ListItemTrack, ModalDialogTrack, ModalDialogAlbum },
+  components: { ContentWithHero, ListItemTrack, ModalDialogTrack, ModalDialogAlbum, CoverArtwork },
 
   data () {
     return {
