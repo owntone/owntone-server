e86aa95d890d325f358d676ff275f82d43267f17
chme <christian.meffert@googlemail.com>
2020-08-23 08:08:22 +0200

    [settings] Add new setting for "webinterface"
    - add settings to show/hide menu items in the top nav bar
    - add setting to show/hide cover artwork in album lists

--- src/settings.c
+++ src/settings.c
@@ -33,6 +33,14 @@ static struct settings_option webinterface_options[] =
   {
       { "show_composer_now_playing", SETTINGS_TYPE_BOOL },
       { "show_composer_for_genre", SETTINGS_TYPE_STR },
+      { "show_cover_artwork_in_album_lists", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_playlists", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_music", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_podcasts", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_audiobooks", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_radio", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_files", SETTINGS_TYPE_BOOL },
+      { "show_menu_item_search", SETTINGS_TYPE_BOOL },
   };
 
 static struct settings_option artwork_options[] =
