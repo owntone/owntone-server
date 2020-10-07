8bc3fcd1091660c3dea42d4045b7d0b3e6a9b510
chme <christian.meffert@googlemail.com>
2020-10-07 09:03:02 +0200

    [web-src] Refine hide/show index lists

--- web-src/src/main.js
+++ web-src/src/main.js
@@ -8,6 +8,8 @@ import './filter'
 import './progress'
 import vClickOutside from 'v-click-outside'
 import VueTinyLazyloadImg from 'vue-tiny-lazyload-img'
+import VueObserveVisibility from 'vue-observe-visibility'
+import VueScrollTo from 'vue-scrollto'
 import 'mdi/css/materialdesignicons.css'
 import 'vue-range-slider/dist/vue-range-slider.css'
 import './mystyles.scss'
@@ -15,6 +17,8 @@ Vue.config.productionTip = false
 
 Vue.use(vClickOutside)
 Vue.use(VueTinyLazyloadImg)
+Vue.use(VueObserveVisibility)
+Vue.use(VueScrollTo)
 
 /* eslint-disable no-new */
 new Vue({
