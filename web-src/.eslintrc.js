d7f1c13585ebc25292d613d935aeac6688d549b1
chme <christian.meffert@googlemail.com>
2022-02-19 06:33:31 +0100

    [web] Configure eslint and prettier dev tools

---
+++ web-src/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    }
+  }
