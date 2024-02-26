50697200c47b78aa19960047304bbc83c6e14d47
Alain Nussbaumer <alain.nussbaumer@alleluia.ch>
2024-02-26 20:33:41 +0100

    [web] Remove useless CSS style

--- web-src/src/templates/ContentWithHero.vue
+++ web-src/src/templates/ContentWithHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero is-light is-bold">
+  <section class="hero is-light">
     <div class="hero-body">
       <div class="container">
         <div class="columns is-centered">
