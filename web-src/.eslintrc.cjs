bd9c844284a670b9d8312b238f5e58b0253d9f57
Alain Nussbaumer <alain.nussbaumer@alleluia.ch>
2024-02-28 15:53:58 +0100

    [web] Enforce more linting rules

--- web-src/.eslintrc.cjs
+++ web-src/.eslintrc.cjs
@@ -4,11 +4,8 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    // Override/add rules settings here, such as:
     'no-unused-vars': ['error', { args: 'none' }],
-    'vue/require-prop-types': 'off',
-    'vue/require-default-prop': 'off',
     'vue/prop-name-casing': ['warn', 'snake_case']
   }
 }
