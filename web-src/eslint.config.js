d6391621a00226968bc62960d1000c6ea8a0b9e0
Alain Nussbaumer <alain.nussbaumer@alleluia.ch>
2024-04-21 17:48:14 +0200

    [web] Upgrade to eslint 9.1.0

---
+++ web-src/eslint.config.js
@@ -0,0 +1,51 @@
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  {
+    files: ['src/**/*.js', 'src/**/.vue'],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  eslintConfigPrettier,
+  js.configs.all,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    rules: {
+      camelcase: 'off',
+      'consistent-this': 'off',
+      'default-param-last': 'off',
+      'id-length': 'off',
+      'max-lines': 'off',
+      'max-lines-per-function': 'off',
+      'max-statements': 'off',
+      'no-bitwise': 'off',
+      'no-magic-numbers': 'off',
+      'no-negated-condition': 'off',
+      'no-nested-ternary': 'off',
+      'no-plusplus': 'off',
+      'no-shadow': 'off',
+      'no-ternary': 'off',
+      'no-unused-vars': ['error', { args: 'none', caughtErrors: 'none' }],
+      'no-undef': 'off',
+      'no-undefined': 'off',
+      'no-unused-expressions': 'off',
+      'no-useless-assignment': 'off',
+      'one-var': 'off',
+      'prefer-destructuring': 'off',
+      'prefer-named-capture-group': 'off',
+      'sort-keys': 'off',
+      'sort-vars': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/no-unused-vars': 'off',
+      'vue/prop-name-casing': 'off',
+      'vue/singleline-html-element-content-newline': 'off'
+    }
+  }
+]
