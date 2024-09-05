1019b777c3744984e03abffd12f89b68cf6c066a
Fabian Groffen <grobian@gentoo.org>
2024-09-05 14:12:45 +0200

    [mpd] outputs: drop invalid outputsvolume key
    This key confuses some clients, and it isn't emitted by MPD, nor
    documented to exist:
    https://github.com/MusicPlayerDaemon/MPD/blob/9ff8e02e543ede2b1964e5ea3f26f00bf2ff90ec/src/output/Print.cxx
    
    It was added in bdb2c7493, but without explanation why it was added to
    outputs command.
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- src/mpd.c
+++ src/mpd.c
@@ -3214,8 +3214,7 @@ mpd_command_toggleoutput(struct mpd_command_output *out, struct mpd_command_inpu
  *   outputname: Computer
  *   plugin: alsa
  *   outputenabled: 1
- *   outputvolume: 50
- * https://mpd.readthedocs.io/en/latest/protocol.html#audio-output-devices
+ * https://mpd.readthedocs.io/en/latest/protocol.html#command-outputs
  */
 static void
 speaker_enum_cb(struct player_speaker_info *spk, void *arg)
@@ -3240,13 +3239,11 @@ speaker_enum_cb(struct player_speaker_info *spk, void *arg)
 		      "outputid: %u\n"
 		      "outputname: %s\n"
 		      "plugin: %s\n"
-		      "outputenabled: %d\n"
-		      "outputvolume: %d\n",
+		      "outputenabled: %d\n",
 		      param->nextid,
 		      spk->name,
 		      plugin,
-		      spk->selected,
-		      spk->absvol);
+		      spk->selected);
   param->nextid++;
 }
 
