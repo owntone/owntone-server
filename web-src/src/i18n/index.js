4694d465082046e36c7313e4ffc0b6b1f7447257
Alain Nussbaumer <alain.nussbaumer@alleluia.ch>
2025-03-01 07:52:48 +0100

    [web] Simplify the declaration of locales

--- web-src/src/i18n/index.js
+++ web-src/src/i18n/index.js
@@ -7,7 +7,7 @@ import { createI18n } from 'vue-i18n'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
 export default createI18n({
-  availableLocales: ('de', 'en', 'fr', 'zh-CN', 'zh-TW'),
+  availableLocales: Object.keys(messages),
   fallbackLocale: 'en',
   fallbackWarn: false,
   legacy: false,
