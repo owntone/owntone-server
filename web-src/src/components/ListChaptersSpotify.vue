331eef6faf488503ab639faa10b02a057172a05a
Jonny Harris <jonathan.harris@news.co.uk>
2026-05-16 23:15:25 +0100

    Add Spotify integration for audiobooks
    - Dynamically add Spotify tab in audiobooks if Spotify service is active.
    - Implement pages and components for Spotify audiobooks, saved audiobooks, and audiobook albums.
    - Extend search functionality to include Spotify audiobooks.
    - Define new routes and localization strings for Spotify audiobook features.

---
+++ web-src/src/components/ListChaptersSpotify.vue
@@ -0,0 +1,63 @@
+<template>
+  <list-item
+    v-for="(item, index) in items"
+    :key="item.id"
+    :is-playable="item.is_playable !== false"
+    :lines="[
+      item.name,
+      item.description ? truncate(item.description, 80) : ''
+    ]"
+    @open="open(item, index)"
+    @open-details="openDetails(item)"
+  >
+    <template v-if="item.is_playable === false" #reason>
+      (<span v-text="$t('list.spotify.not-playable-track')" />
+      <span
+        v-if="item.restrictions?.reason"
+        v-text="
+          $t('list.spotify.restriction-reason', {
+            reason: item.restrictions.reason
+          })
+        "
+      />)
+    </template>
+  </list-item>
+  <loader-list-item :load="load" />
+  <modal-dialog-track-spotify
+    :item="selectedItem"
+    :show="showDetailsModal"
+    @close="showDetailsModal = false"
+  />
+</template>
+
+<script setup>
+import ListItem from '@/components/ListItem.vue'
+import LoaderListItem from '@/components/LoaderListItem.vue'
+import ModalDialogTrackSpotify from '@/components/ModalDialogTrackSpotify.vue'
+import queue from '@/api/queue'
+import { ref } from 'vue'
+
+const props = defineProps({
+  contextUri: { default: '', type: String },
+  items: { required: true, type: Object },
+  load: { default: null, type: Function }
+})
+
+const selectedItem = ref({})
+const showDetailsModal = ref(false)
+
+const truncate = (text, length) =>
+  text.length > length ? `${text.substring(0, length)}…` : text
+
+const open = (item, index) => {
+  if (item.is_playable !== false) {
+    queue.playUri(props.contextUri || item.uri, false, index)
+  }
+}
+
+const openDetails = (item) => {
+  selectedItem.value = item
+  showDetailsModal.value = true
+}
+</script>
+
